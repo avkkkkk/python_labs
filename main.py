625cc8972a34c8201769ef3b929303e9de0f1283
avkkkkk <alexandr_kusnetsov0808@bk.ru>
2026-09-22 20:38:18 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+import platform
+import sys
+import os
+import getpass
+import uuid
+
+result = {
+    "system": platform.platform(),
+    "release": platform.release(),
+    "version": platform.version(),
+    "hostname": platform.node(),
+    "python_version": platform.python_version(),
+    "interpreter_path": sys.executable,
+    "cpu_count": os.cpu_count(),
+    "username": getpass.getuser(),
+}
+with open("result.json", "w") as f:
+    json.dump(result, f, indent=4)
